5962b98c667341e154362a13bc5f38a27bda0993
Rafael <rafaelmoreiracvo@yahoo.com.br>
2019-10-11 08:56:05 -0300

    atualizando

---
+++ house_prices.R
@@ -0,0 +1,14 @@
+# Desafio do Kaggle sobre House Prices
+
+# setando o diretorio
+setwd("~/git/house_prices")
+
+# Lendo o arquivo
+
+sample <- read.csv('datasets/sample_submission.csv')
+train <- read.csv('datasets/train.csv')
+test <- read.csv('datasets/test.csv')
+
+
+
+
